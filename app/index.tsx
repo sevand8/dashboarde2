8227d66af55e9480b9a93e80b6789d9debfb8e7a
seb.and156 <seb.and156@gmail.com>
2025-09-17 17:38:05 +0000

    💄ui(ProfileDetails): Crear los detalles principales del usuario. - Añadir imagen de perfil. - Añadir nombre y profesion del usuario.

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,13 @@
 import { Text, View } from "react-native";
+import ProfileDetails from "@/components/ProfileDetails";
 import "../global.css";
 export default function Index() {
   return (
     <View className="flex justify-center items-center bg-white-800 w-screen h-screen">
-      <Text className="text-black bold text-xl">Edit app/index.tsx to edit this screen.</Text>
-      
+      <Text className="text-black bold text-xl"></Text>
+      <View className="flex justify-center items-center bg-white w-screen h-screen">
+      <ProfileDetails></ProfileDetails>
+    </View>
     </View>
   );
 }
--- components/ProfileDetails.tsx
+++ components/ProfileDetails.tsx
@@ -1,12 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Image, Text } from 'react-native'
 import React from 'react'
+import "../global.css";
 
 const ProfileDetails = () => {
   return (
-    <View>
-      <Text>ProfileDetails</Text>
+    <View className='flex justify-center items-center bg-white w-screen h-screen'>
+      < Image source ={require("../assets/images/icon.png")} 
+      className="max-w-32 max-h-32 rounded-full"
+      />
+      <Text className='text-black bold text-xl'> Sebastian Andrade </Text>
+      <Text className='font-bold text-gray-500'>📕Estudiante📕</Text>
+
+
     </View>
   )
+
 }
 
 export default ProfileDetails
